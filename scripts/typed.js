c4fe43da96aea9309ae26e9d7714a779b6bd1d44
prmnaa30 <73104057+prmnaa30@users.noreply.github.com>
2023-12-17 00:18:37 +0800

    first commit

---
+++ scripts/typed.js
@@ -0,0 +1,10 @@
+function typedjs() { 
+    var typed = new Typed('#typed', {
+        strings: ['College Student.', '^1000Beginner FE Developer.', '^1000'],
+        typeSpeed: 100,
+        loop: true,
+        loopCount: Infinity,
+        backDelay: 2000,
+    });
+}
+typedjs()
